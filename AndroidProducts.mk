2e4e0efb8824b0b0679fe323b02c435554ee56f3
YKW.awaitzz <111608769+lil-awaitz@users.noreply.github.com>
2023-02-10 22:24:04 +0100

    bringup omni

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,5 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_veux.mk
+	$(LOCAL_DIR)/omni_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_veux-user \
-    lineage_veux-userdebug \
-    lineage_veux-eng
+    $(foreach variant, user userdebug eng, omni_veux-$(variant))
