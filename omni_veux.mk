305d48c27e12a278b1a60238a6020257857f8690
YKW.awaitzz <111608769+lil-awaitz@users.noreply.github.com>
2023-02-10 22:21:21 +0100

    bringup omni

--- omni_veux.mk
+++ omni_veux.mk
@@ -4,12 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+TARGET_BOOTANIMATION_SIZE := 1080p
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
